d680e26c607fdd54d13bb2f38dab4625b06f48f4
seve.hooker@gmail.com <seve.hooker@gmail.com>
2020-12-02 21:34:26 -0600

    Add data

--- Day1/src/Day1.fs
+++ Day1/src/Day1.fs
@@ -1,5 +1,9 @@
 ﻿namespace Day1
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Day1 =
+
+    let result values =
+        for i = 0 to 1010 do
+            match i with
+            | value when List.exists (fun value -> value = i) values && List.exists (fun value -> 2020-i = value) values -> printfn "%d" (value*(2020-value))
+            // | _ -> skip
